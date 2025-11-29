d44e429906ba183c14ef54b1d1c4cd3943789e3d
SPST - Wine Connect <spediamoallestero@gmail.com>
2025-11-29 13:38:42 +0100

    Update index.ts

--- lib/docs/render/index.ts
+++ lib/docs/render/index.ts
@@ -1,14 +1,17 @@
 // lib/docs/render/index.ts
 import type { DocData } from "./types";
+import { renderCommercialeHtml } from "./commerciale";
 import { renderProformaHtml } from "./proforma";
 
 export function renderDocumentHtml(doc: DocData): string | null {
   switch (doc.meta.docType) {
     case "fattura_proforma":
-    case "fattura_commerciale":
       return renderProformaHtml(doc);
+
+    case "fattura_commerciale":
+      return renderCommercialeHtml(doc);
+
     default:
-      // DDT / DLE li aggiungeremo qui in seguito
       return null;
   }
 }
