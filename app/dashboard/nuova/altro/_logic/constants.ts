2c0aebf7a6772699462164eeda300077d9c45edd
SPST - Wine Connect <spediamoallestero@gmail.com>
2025-12-27 13:39:52 +0100

    Create constants.ts

---
+++ app/dashboard/nuova/altro/_logic/constants.ts
@@ -0,0 +1,3 @@
+// FILE: app/dashboard/nuova/altro/_logic/constants.ts
+export const GMAPS_LANG = process.env.NEXT_PUBLIC_GOOGLE_MAPS_LANGUAGE || "it";
+export const GMAPS_REGION = process.env.NEXT_PUBLIC_GOOGLE_MAPS_REGION || "IT";
